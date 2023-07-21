873172a7957542df211552017a13b95a7909c789
Md Robin Rana <developer.smrobin45@gmail.com>
2023-07-21 16:54:47 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        container: {
+            center: true,
+            padding: "5rem",
+        },
+        extend: {},
+    },
+    plugins: [],
+};
